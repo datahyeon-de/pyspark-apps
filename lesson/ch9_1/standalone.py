c5e79e2534a0454f4074e2d0c51229140d2053d6
datahyeon <hyeondata@gmail.com>
2025-09-12 13:15:37 +0900

    ch9.1 작성

---
+++ lesson/ch9_1/standalone.py
@@ -0,0 +1,19 @@
+from pyspark.sql import SparkSession
+import time
+
+spark = SparkSession \
+    .builder \
+    .appName('standalone') \
+    .getOrCreate()
+
+schema = 'ID INT, COUNTRY STRING, HIT LONG'
+df = spark.createDataFrame(data=[(1, 'Korea', 120),
+                                 (2, 'USA', 80),
+                                 (3, 'Japan', 40)],
+                           schema=schema
+                           )
+df.show()
+print('=============')
+df.count()
+
+time.sleep(300)
