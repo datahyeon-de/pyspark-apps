1547c95a21e5ad3523057f1b54583a44f8757b2c
datahyeon <hyeondata@gmail.com>
2025-09-18 12:11:03 +0900

    ch12.2 작성

---
+++ lesson/ch12_2/kafka_streaming_base.py
@@ -0,0 +1,20 @@
+from pyspark.sql import SparkSession
+
+app_name = 'kafka_streaming_base'
+spark = SparkSession.builder.appName(app_name).getOrCreate()
+
+kafka_read_option = {
+    'bootstrap.servers': "kafka01:9092,kafka02:9092,kafka03:9092",
+    'subscribe': 'lesson.spark-streaming.test',
+}
+
+kafka_source_df = spark.readStream.format('kafka').option(**kafka_read_option).load()
+
+kafka_write_option = {
+    'checkpointLocation': f'/home/spark/kafka_offsets/{app_name}',
+    'truncate': 'false'
+}
+
+query = kafka_source_df.writeStream.format('console').option(**kafka_write_option).start()
+
+query.awaitTermination()
